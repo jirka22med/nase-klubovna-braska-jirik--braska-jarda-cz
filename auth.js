3d82fee9c2646b1d05001782ed75e343afac3a9a
jirka22med <153131598+jirka22med@users.noreply.github.com>
2026-09-06 08:18:53 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -31,7 +31,7 @@ setPersistence(auth, browserLocalPersistence).catch((err) => {
 // ╚══════════════════════════════════════════════════╝
 const POVOLENI_UZIVATELE = [
   "jirkamed66@gmail.com",
-  "jirmedokura@post.cz",
+  "jirmedokura@gmail.com",
   "jakesjardajak@gmail.com"    // ← ⚠️ DOPLNIT!
 ];
 
